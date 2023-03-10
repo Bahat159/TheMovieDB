b450e85fffa8fd3fd4e983dd8c1d6a3b52c7ea01
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2023-03-10 11:16:32 -0600

    Update models.py

--- themoviedb/models.py
+++ themoviedb/models.py
@@ -1,1 +1,12 @@
 
+from django.db import models
+
+# Create your models here.
+
+class ListItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    language = models.TextField()
+
+    def __str__(self):
+        return self.name
