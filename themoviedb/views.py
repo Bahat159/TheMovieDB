ca077b8a32504d84592a225b081cbdb16417cc63
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2023-03-10 11:23:18 -0600

    Update views.py

--- themoviedb/views.py
+++ themoviedb/views.py
@@ -9,6 +9,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from django.http import HttpResponse, JsonResponse
 
+from .models import ListItem
+
 
 API_KEY = "fc5462f5a436d1d6139b13070b1d3d03"
 
@@ -16,10 +18,11 @@ API_KEY = "fc5462f5a436d1d6139b13070b1d3d03"
 base_url = 'https://api.themoviedb.org/3'
 
 
-discover_movie_url = '/discover/movie?api_key='+API_KEY
-discover_tv_url = '/discover/tv?api_key='+API_KEY
-genre_movie_url = '/genre/movie/list?api_key='+API_KEY
-genre_tv_url = '/genre/tv/list?api_key='+API_KEY
+discover_movie_url = f'/discover/movie?api_key={API_KEY}'
+discover_tv_url = f'/discover/tv?api_key={API_KEY}'
+genre_movie_url = f'/genre/movie/list?api_key={API_KEY}'
+genre_tv_url = f'/genre/tv/list?api_key={API_KEY}'
+trending_url = f'/trending/all/day?api_key={API_KEY}'
 
 
 
@@ -31,7 +34,7 @@ def discover_all_movie(request):
         try:
             my_request = requests.get(movie_url)
             if my_request.ok:
-                parse_data = my_request.json()    
+                parse_data = my_request.json()
                 return JsonResponse(parse_data, status=status.HTTP_201_CREATED)
             else:
                 return HttpResponse(my_request.status_code)
@@ -92,8 +95,8 @@ def genre_tv_list(request):
         return HttpResponse('Exception Occured')
 
 
-def get_movie_id(request, movie_id):
-    movie_id_url = base_url+'/movie/{movie_id}?api_key='+API_KEY
+def get_movie_id(request, movie_id, page_number):
+    movie_id_url = base_url+f'/movie/{movie_id}?api_key={API_KEY}&page={page_number}'
     try:
         my_request = requests.get(movie_id_url)
         if my_request.ok:
@@ -114,5 +117,45 @@ def get_movie_credit(request, movie_id):
             return JsonResponse(parse_data, status=status.HTTP_201_CREATED)
         else:
             return HttpResponse('Status Code: >> '+my_request.status_code)
+    except:
+        return HttpResponse('Exception Occured')
+
+
+
+def get_list(request, list_id):
+    list_url = base_url+'/list/{list_id}?api_key='+API_KEY
+    try:
+        my_request = requests.get(list_url)
+        if my_request.ok:
+            parse_data = my_request.json()
+            return JsonResponse(parse_data, status=status.HTTP_201_CREATED)
+        else:
+            return HttpResponse('Status Code: >> '+my_request.status_code)
+    except:
+        return HttpResponse('Exception Occured')
+
+
+def get_list_item_status(request, list_id):
+    list_item_url = base_url+'/list/{list_id}/item_status?api_key='+API_KEY
+    try:
+        my_request = requests.get(list_item_url)
+        if my_request.ok:
+            parse_data = my_request.json()
+            return JsonResponse(parse_data, status=status.HTTP_201_CREATED)
+        else:
+            return HttpResponse('Status Code: >> '+my_request.status_code)
+    except:
+        return HttpResponse('Exception Occured')
+
+
+def get_trending(request):
+    trend_url = base_url+trending_url
+    try:
+        my_request = requests.get(trend_url)
+        if my_request.ok:
+            parse_data = my_request.json()
+            return JsonResponse(parse_data, status=status.HTTP_201_CREATED)
+        else:
+            return HttpResponse(my_request.status_code)
     except:
         return HttpResponse('Exception Occured')
