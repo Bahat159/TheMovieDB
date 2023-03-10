ed07ecdd091d9e2f7fdad68d9209fd815a347e1b
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2023-03-10 11:46:32 -0600

    Update views.py

--- themoviedb/views.py
+++ themoviedb/views.py
@@ -56,10 +56,10 @@ def discover_tv_details(request):
 
 
 def find_movie(requests, external_id):
-    find_url = base_url+'/find/{external_id}?api_key='+API_KEY
+    finder_url = base_url+f'/find/{external_id}?api_key={API_KEY}'
 
     try:
-        my_request = requests.get(find_url)
+        my_request = requests.get(finder_url)
         if my_request.ok:
             parse_data = my_request.json()
             return JsonResponse(parse_data, status=status.HTTP_201_CREATED)
@@ -109,7 +109,7 @@ def get_movie_id(request, movie_id, page_number):
 
 
 def get_movie_credit(request, movie_id):
-    movie_credit_url = base_url+'/movie/{movie_id}/credits?api_key='+API_KEY
+    movie_credit_url = base_url+f'/movie/{movie_id}/credits?api_key={API_KEY}'
     try:
         my_request = requests.get(movie_credit_url)
         if my_request.ok:
@@ -123,7 +123,7 @@ def get_movie_credit(request, movie_id):
 
 
 def get_list(request, list_id):
-    list_url = base_url+'/list/{list_id}?api_key='+API_KEY
+    list_url = base_url+f'/list/{list_id}?api_key={API_KEY}'
     try:
         my_request = requests.get(list_url)
         if my_request.ok:
@@ -136,9 +136,31 @@ def get_list(request, list_id):
 
 
 def get_list_item_status(request, list_id):
-    list_item_url = base_url+'/list/{list_id}/item_status?api_key='+API_KEY
+    list_item_status_url = base_url+f'/list/{list_id}/item_status?api_key={API_KEY}'
     try:
-        my_request = requests.get(list_item_url)
+        my_request = requests.get(list_item_status_url)
+        if my_request.ok:
+            parse_data = my_request.json()
+            return JsonResponse(parse_data, status=status.HTTP_201_CREATED)
+        else:
+            return HttpResponse('Status Code: >> '+my_request.status_code)
+    except:
+        return HttpResponse('Exception Occured')
+
+
+def create_list(request):
+    list_item_url = base_url+f'/list/?api_key={API_KEY}'
+
+    try:
+        """
+        list_curator = ListItem(name="Test Data", description="List Item Creation", language="English")
+        list_curator.save()
+        if list_curator:
+            print('Success saving record to db')
+        """
+        listcurator = {"name": "myname","description": "my description","language": "my language"}
+        my_request = requests.post(list_item_url, data=listcurator)
+    
         if my_request.ok:
             parse_data = my_request.json()
             return JsonResponse(parse_data, status=status.HTTP_201_CREATED)
@@ -152,6 +174,19 @@ def get_trending(request):
     trend_url = base_url+trending_url
     try:
         my_request = requests.get(trend_url)
+        if my_request.ok:
+            parse_data = my_request.json()
+            return JsonResponse(parse_data, status=status.HTTP_201_CREATED)
+        else:
+            return HttpResponse(my_request.status_code)
+    except:
+        return HttpResponse('Exception Occured')
+
+
+def get_review(request, review_id):
+    review_url = base_url+f'/review/{review_id}?api_key={API_KEY}'
+    try:
+        my_request = requests.get(review_url)
         if my_request.ok:
             parse_data = my_request.json()
             return JsonResponse(parse_data, status=status.HTTP_201_CREATED)
