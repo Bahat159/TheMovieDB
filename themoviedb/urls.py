6992e2a9828aa297ae0679b212c3c5d7ee817ee9
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2023-03-10 11:24:13 -0600

    Update urls.py

--- themoviedb/urls.py
+++ themoviedb/urls.py
@@ -17,8 +17,10 @@ urlpatterns = [
     path('movie/<int:movie_id>/<int:page_number>', views.get_movie_id,  name='Movie ID'),
     path('movie/<int:movie_id>/credits', views.get_movie_credit,  name='Movie Credits'),
 
-    path('list/', views.create_list,  name='Create List'),
     path('list/<int:list_id>', views.get_list,  name='Get List'),
     path('list/<int:list_id>/status', views.get_list_item_status,  name='Get List Item Status'),
+
+    path('trending/', views.get_trending,  name='Get Trending'),
+
 
 ]
